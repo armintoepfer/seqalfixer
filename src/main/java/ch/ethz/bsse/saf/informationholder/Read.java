d79b8c9e41ea9de4755f554cfa514863c99fe666
armintoepfer <armin.toepfer@gmail.com>
2013-08-26 17:45:13 +0200

    Naming

--- src/main/java/ch/ethz/bsse/saf/informationholder/Read.java
+++ src/main/java/ch/ethz/bsse/saf/informationholder/Read.java
@@ -1,19 +1,19 @@
 /**
  * Copyright (c) 2013 Armin Töpfer
  *
- * This file is part of AlignmentFixer.
+ * This file is part of SequenceAlignmentFixer.
  *
- * AlignmentFixer is free software: you can redistribute it and/or modify it
+ * SequenceAlignmentFixer is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation, either version 3 of the License, or any later version.
  *
- * AlignmentFixer is distributed in the hope that it will be useful, but WITHOUT
+ * SequenceAlignmentFixer is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
  *
  * You should have received a copy of the GNU General Public License along with
- * AlignmentFixer. If not, see <http://www.gnu.org/licenses/>.
+ * SequenceAlignmentFixer. If not, see <http://www.gnu.org/licenses/>.
  */
 package ch.ethz.bsse.saf.informationholder;
 
