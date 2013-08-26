4f3160e45cb08f36101cec8f944e5836e28d7c61
armintoepfer <armin.toepfer@gmail.com>
2013-08-26 15:25:44 +0200

    Insertions with minimum occurrence of -consensus INT are integrated in consensus. Same insertions in window of 20bp are merged.

--- src/main/java/ch/ethz/bsse/saf/insertion/Insertions.java
+++ src/main/java/ch/ethz/bsse/saf/insertion/Insertions.java
@@ -4,7 +4,9 @@
  */
 package ch.ethz.bsse.saf.insertion;
 
+import ch.ethz.bsse.saf.informationholder.Globals;
 import ch.ethz.bsse.saf.informationholder.Read;
+import ch.ethz.bsse.saf.utils.Utils;
 import com.google.common.collect.HashMultiset;
 import com.google.common.collect.Multiset;
 import java.util.Arrays;
@@ -12,7 +14,6 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 /**
  *
@@ -50,8 +51,65 @@ public class Insertions {
                 if (!this.insertionTriple.containsKey(e.getElement().position)) {
                     this.insertionTriple.put(e.getElement().position, new LinkedList());
                 }
-                List<InsertionTriple> list = this.insertionTriple.get(e.getElement().position);
-                list.add(new InsertionTriple(e.getElement().position, e.getElement().sequence, e.getCount()));
+                this.insertionTriple.get(e.getElement().position).add(new InsertionTriple(e.getElement().position, e.getElement().sequence, e.getCount()));
+            }
+            Map<String, List<InsertionTriple>> insertSequenceToTriple = new HashMap<>();
+            for (Map.Entry<Integer, List<InsertionTriple>> e : this.insertionTriple.entrySet()) {
+                for (InsertionTriple it : e.getValue()) {
+                    if (!insertSequenceToTriple.containsKey(Utils.reverse(it.sequence))) {
+                        insertSequenceToTriple.put(Utils.reverse(it.sequence), new LinkedList());
+                    }
+                    insertSequenceToTriple.get(Utils.reverse(it.sequence)).add(it);
+                }
+            }
+
+            for (Map.Entry<String, List<InsertionTriple>> e : insertSequenceToTriple.entrySet()) {
+                if (e.getValue().size() == 1) {
+                    if (e.getValue().get(0).count < 10) {
+                        e.getValue().clear();
+                    }
+                    continue;
+                }
+                int maxTmp = 0;
+                for (InsertionTriple it : e.getValue()) {
+                    if (it.count > maxTmp) {
+                        maxTmp = it.count;
+                    }
+                }
+                if (maxTmp < 10) {
+                    continue;
+                }
+                List<InsertionTriple> merged = new LinkedList<>();
+                while (merged.size() != e.getValue().size()) {
+                    int max = 0;
+                    InsertionTriple it_tmp = null;
+                    for (InsertionTriple it : e.getValue()) {
+                        if (it.count > max && !merged.contains(it)) {
+                            max = it.count;
+                            it_tmp = it;
+                        }
+                    }
+                    List<InsertionTriple> merger = new LinkedList<>();
+                    for (InsertionTriple it : e.getValue()) {
+                        if (it.position > it.position - 10 && it.position < it.position + 10 && !it.equals(it_tmp)) {
+                            merger.add(it);
+                            it_tmp.count += it.count;
+                        }
+                    }
+                    e.getValue().removeAll(merger);
+                    merged.add(it_tmp);
+                }
+            }
+            insertionTriple = new HashMap<>();
+            for (Map.Entry<String, List<InsertionTriple>> e : insertSequenceToTriple.entrySet()) {
+                if (!e.getValue().isEmpty()) {
+                    for (InsertionTriple it : e.getValue()) {
+                        if (!this.insertionTriple.containsKey(it.position)) {
+                            this.insertionTriple.put(it.position, new LinkedList());
+                        }
+                        this.insertionTriple.get(it.position).add(new InsertionTriple(it.position, it.sequence, it.count));
+                    }
+                }
             }
         }
         return insertionTriple;
